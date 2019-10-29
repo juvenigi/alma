3e77aa204c74c5983ad141c9fc5df013d5f976a1
s6dmsolo <s6dmsolo@cip14.iam.uni-bonn.de>
2019-10-29 14:36:26 +0100

    code cleanup

--- src/bin_dec_arr.cpp
+++ src/bin_dec_arr.cpp
@@ -48,13 +48,9 @@ int * int_decarr(int number, OUT int * length){
 bool * int_boolarr(int value, OUT int * length){
   *length = bilen(value);
   bool * array = (bool *) malloc(sizeof(bool) * (*length));
-  for(int i = *length-1; i>0; i--){
-    if(value - pow(2,i)>=0){
-      value = value - pow(2,i);
-      array[i] = 1;
-    }
-    if(value - 1 == 0)
-      array[0] = 1;
+  for(int i = 0; i < *length; i++){
+    array[i] = value % 2;
+    value /= 2;
   }
   return array;
 }
@@ -130,8 +126,8 @@ bool * sum_boolarr(bool * bool1, bool * bool2, int len1, int len2, OUT int * out
   for(int i = 0; i<len2; i++){
     bool2b[i] = bool2[i];
   }
-  carryr[0] = 1;
-  while(!isZero(carryr,*outlen)){
+  
+  do{
     for(int i = 0; i<*outlen; i++){
       carryr[i] = outarr[i] & bool2b[i];
       outarr[i] = outarr[i] ^ bool2b[i];
@@ -140,28 +136,46 @@ bool * sum_boolarr(bool * bool1, bool * bool2, int len1, int len2, OUT int * out
     for(int i=0; i<*outlen; i++){
       bool2b[i] = carryr[i];
     }
-  }
+  }while(!isZero(carryr,*outlen));
   return outarr;
 }
 
 
 int main(void){
 
-  int n1 = 20;
+  int n1 = 20; // for testing purpposes only
   int n2 = 69;
+  cin >> n1;
+  cin >> n2;
+
+  n1 = abs(n1); // code sanitation
+  n2 = abs(n2);
+
+  
   int nb1_l;
   int nb2_l;
   bool * nb1 = int_boolarr(n1,&nb1_l);
   bool * nb2 = int_boolarr(n2,&nb2_l);
-
   int nbs_l;
   bool * nbs = sum_boolarr(nb1,nb2,nb1_l,nb2_l,&nbs_l);
 
+  int ni1_l;
+  int ni2_l;
+  int * ni1 = int_decarr(n1,&ni1_l);
+  int * ni2 = int_decarr(n2,&ni2_l);
+
+  int nis_l;
+  int * nis = sum_decarr(ni1,ni2,ni1_l,ni2_l,&nis_l);
+  
   cout << n1 << " " << n2 << endl;
 
   printbool(nb1,nbs_l);
   printbool(nb2,nbs_l);
   printbool(nbs,nbs_l);
+
+  printdec(ni1,nis_l);
+  printdec(ni2,nis_l);
+  printdec(nis,nis_l);
 
   return 0;
 }

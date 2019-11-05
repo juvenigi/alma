d1de9a85b8aa0ded82c3a406e76e069ff1d35278
joghourt <juvenigi@gmail.com>
2019-11-05 10:49:21 +0100

    cleanup

--- src/ter-sym.cpp
+++ src/ter-sym.cpp
@@ -3,7 +3,9 @@
 
 using namespace std;
 
+#define OUT
 
+/* Helper Functions */
 void printdig(pair<bool,bool> pair){
   if(pair.second == true){cout << '1';}else{ cout << '0';} // one or zero
   if(pair.first == false && pair.second == true) {cout << "\xCC\x85";} // add a hat only to one
@@ -16,7 +18,8 @@ void printt(pair<bool,bool> * arr, int length){ // simple recursion
   cout << endl;
 }
 
-void flipsign(pair<bool,bool> * num, int length){ // -1 becomes 1 and vice versa
+/* Additive inverse involves flipping the sign of every digit */
+void flipsign(pair<bool,bool> * num, int length){
   for(int i = 0; i<length; i++){
     switch(num[i].first){
 
@@ -30,19 +33,27 @@ void flipsign(pair<bool,bool> * num, int length){ // -1 becomes 1 and vice versa
   }
 }
 
-pair<bool,bool> * conv_dec(int num){
-  int length = 4; // lazy, it is better to check for the nearest 2*3^n
-  pair<bool,bool> * out = new pair<bool,bool>[length]; // {{bool,bool},{bool,bool}...} a bool pair array
+int terlen(int decnum){
+  if(!decnum){return 1;}
+  int i = 0;
+  while(pow(3,i)<2*abs(decnum)){++i;}
+  return i;
+}
+
+pair<bool,bool> * conv_dec(int num, OUT int * length){
+  //int length = 4; // lazy, it is better to check for the nearest 2*3^n
+  *length = terlen(num);
+  pair<bool,bool> * out = new pair<bool,bool>[*length]; // {{bool,bool},{bool,bool}...} a bool pair array
 
   if(num == 0) {out[0].first = false; out[0].second = false; return out;} // zero is simple
 
   bool pos = true;
   if(num < 0){ // do the following if the number is negative
-    pos = false;
+    pos = false; // will run flipsign later
     num = 0 - num;
   }
   int i = 0;
-  while(i<length){
+  while(i<*length){
     switch(num % 3){
     case 1:
       out[i].first = true;
@@ -64,29 +75,31 @@ pair<bool,bool> * conv_dec(int num){
     i++;
     num /= 3;
   }
-  if(!pos){flipsign(out,length);}
+  if(!pos){flipsign(out,*length);}
     return out;
 }
 
 
 int main(void){
 
-  pair<bool,bool> pos1 = make_pair(true,true);
-  pair<bool,bool> zero = make_pair(false,false);
-  pair<bool,bool> neg1 = make_pair(false,true);
+  // pair<bool,bool> pos1 = make_pair(true,true);
+  // pair<bool,bool> zero = make_pair(false,false);
+  // pair<bool,bool> neg1 = make_pair(false,true);
 
-  printdig(pos1);
-  cout << endl;
-  printdig(zero);
-  cout << endl;
-  printdig(neg1);
-  cout << endl;
-  cout << endl;
+  // printdig(pos1);
+  // cout << endl;
+  // printdig(zero);
+  // cout << endl;
+  // printdig(neg1);
+  // cout << endl;
+  // cout << endl;
 
   pair<bool,bool> * numa;
-  for(int j = -13; j<15; j++){
-    numa = conv_dec(j);
-    printt(numa,4);
+  int numal;
+  for(int j = -13; j<14; j++){
+    numa = conv_dec(j,&numal);
+    cout << j << " -> ";
+    printt(numa,numal);
   }
   return 0;
 }
